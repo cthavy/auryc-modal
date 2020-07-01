cadf6fe3cbd8b28749fb34d52880d84cbb243ed6
casey <caseythavy@gmail.com>
2020-07-01 11:35:57 -0700

    demo purposes complete

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/**
+ * changing categories of feedback
+ */
 const x = document.getElementById("categories");
 const website = document.getElementById("website");
 const products = document.getElementById("products");
@@ -26,5 +29,42 @@ function changeCategory() {
       break;
     default:
       break;
-  } 
+  }
+}
+
+/**
+ * select state for ratings
+ */
+const ratings = document.getElementsByClassName("rating");
+function rate(self) {
+  for (let i = 0; i < ratings.length; i++) {
+    ratings[i].classList.remove('active');
+    if (i === parseInt(self.dataset.val)) {
+      self.classList.add('active');
+    }
+  }
+}
+
+/**
+ * keeping track of character count within text areas
+ */
+const ta_feed = document.getElementById("ta-feed");
+const ta_loc = document.getElementById("ta-loc");
+const ta_gen = document.getElementsByClassName("ta-gen");
+
+const c_feed = document.getElementById("feed-counter");
+const c_loc = document.getElementById("loc-counter");
+const c_gen = document.getElementsByClassName("gen-counter");
+
+ta_feed.addEventListener("input", countChars.bind(this, ta_feed, c_feed));
+ta_loc.addEventListener("input", countChars.bind(this, ta_loc, c_loc));
+for (let i = 0; i < ta_gen.length; i++) {
+  ta_gen[i].addEventListener('input', countChars.bind(this, ta_gen[i], c_gen[i]));
+}
+
+function countChars(self, counter) {
+  const MAXLENGTH = 1000;
+  let currentLength = self.value.length;
+
+  counter.innerHTML = MAXLENGTH - currentLength + " characters left";
 }
